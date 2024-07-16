6f2712af531f04f90959fcc6fec96f5c9bc0de36
Shampongzhu <xp_zhu@qq.com>
2024-07-16 11:45:09 +0800

    Update pr-review-script.js

--- pr-review-script.js
+++ pr-review-script.js
@@ -9,7 +9,6 @@ const prompt = ["When was Microsoft founded?"];
 async function main() {
   const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });  
   const result = await client.completions.create({ prompt, model: deployment, max_tokens: 128 });
-  console.log('===', result)
   for (const choice of result.choices) {
     console.log(choice.text);
   }
