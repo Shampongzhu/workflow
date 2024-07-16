bd0dc15b4baa4b618972766a5349699670424fb4
Shampongzhu <xp_zhu@qq.com>
2024-07-16 11:27:48 +0800

    Update pr-review-script.js

--- pr-review-script.js
+++ pr-review-script.js
@@ -1,19 +1,32 @@
-const { OpenAIClient, AzureKeyCredential } = require('@azure/openai');
-const OPENAI_APIKEY = process.env.OPENAI_APIKEY
+const { AzureOpenAI } = require("openai");
+
+// Load the .env file if it exists
+const dotenv = require("dotenv");
+dotenv.config();
+
+// You will need to set these environment variables or edit the following values
+const endpoint = 'https://chataihub3097202828.openai.azure.com/';
+const apiKey = process.env.OPENAI_APIKEY;
+const apiVersion = "2024-04-01-preview";
+const deployment = "gpt-35-turbo-instruct"; //The deployment name for your completions API model. The instruct model is the only new model that supports the legacy API.
+
+const prompt = ["When was Microsoft founded?"];
 
 async function main() {
-  const client = new OpenAIClient('https://chataihub3097202828.openai.azure.com/', new AzureKeyCredential(OPENAI_APIKEY));
+  console.log("== Get completions Sample ==");
 
-  const { choices } = await client.getCompletions(
-    'gpt-35-turbo', // assumes a matching model deployment or model name
-    ['Hello, world!'],
-  );
+  const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });  
 
-  for (const choice of choices) {
+  const result = await client.completions.create({ prompt, model: deployment, max_tokens: 128 });
+
+  for (const choice of result.choices) {
     console.log(choice.text);
   }
 }
 
 main().catch((err) => {
-  console.error('The sample encountered an error:', err);
+  console.error("Error occurred:", err);
 });
+
+module.exports = { main };
+
