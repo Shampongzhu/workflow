63d4a3b8ff13b91849a6b79613e7217b44b9dad3
Shampongzhu <xp_zhu@qq.com>
2024-07-16 11:43:13 +0800

    Update pr-review-script.js

--- pr-review-script.js
+++ pr-review-script.js
@@ -1,21 +1,15 @@
 const { AzureOpenAI } = require("openai");
 
-const dotenv = require("dotenv");
-dotenv.config();
-
-const endpoint = 'https://chataihub3097202828.openai.azure.com/';
+const endpoint = process.env.ENDPOINT;
 const apiKey = process.env.OPENAI_APIKEY;
 const apiVersion = "2024-02-01";
 const deployment = "gpt-35-turbo";
 const prompt = ["When was Microsoft founded?"];
 
 async function main() {
-  console.log("== Get completions Sample ==");
-
   const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });  
-
   const result = await client.completions.create({ prompt, model: deployment, max_tokens: 128 });
-
+  console.log('===', result)
   for (const choice of result.choices) {
     console.log(choice.text);
   }
