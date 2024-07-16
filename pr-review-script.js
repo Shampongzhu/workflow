c2f496c7290948ad70a82ce7412968585acbb1ba
Shampongzhu <xp_zhu@qq.com>
2024-07-16 12:34:17 +0800

    Update pr-review-script.js

--- pr-review-script.js
+++ pr-review-script.js
@@ -4,7 +4,6 @@ const endpoint = process.env.ENDPOINT;
 const apiKey = process.env.OPENAI_APIKEY;
 const apiVersion = "2024-02-01";
 const model = "gpt-35-turbo";
-const prompt = ["When was Microsoft founded?"];
 
 async function chat(prompt = []) {
   const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, model });  
