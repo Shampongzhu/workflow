e205e682a03ed82259b129eb299018e81f7e668d
Shampongzhu <xp_zhu@qq.com>
2024-07-16 14:19:43 +0800

    1

--- pr-review-script.js
+++ pr-review-script.js
@@ -1,13 +1,26 @@
 const { AzureOpenAI } = require("openai");
+const { Octokit } = require("@octokit/rest");
 
 const repo = process.env.REPO
 const token = process.env.TOKEN
+const event = process.env.EVENT
 const endpoint = process.env.ENDPOINT;
 const apiKey = process.env.OPENAI_APIKEY;
 const apiVersion = "2024-02-01";
 const model = "gpt-35-turbo";
 
-console.log('===', repo, token)
+const octokit = new Octokit({
+  auth: token
+});
+
+console.log('===', event)
+
+// const commits = await octokit.rest.repos.compareCommits({
+//   owner: 'octocat',
+//   repo: 'Hello-World',
+//   base: 'commit-sha-1', // 基准提交的 SHA
+//   head: 'commit-sha-2'  // 比较提交的 SHA
+// })
 
 async function chat(prompt = []) {
   const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, model });  
