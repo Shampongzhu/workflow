b0e4b19b93d9c797edbd888279313243fb401c29
Shampongzhu <xp_zhu@qq.com>
2024-07-16 14:00:02 +0800

    Update pr-review-script.js

--- pr-review-script.js
+++ pr-review-script.js
@@ -6,7 +6,7 @@ const apiKey = process.env.OPENAI_APIKEY;
 const apiVersion = "2024-02-01";
 const model = "gpt-35-turbo";
 
-console.log('===', github)
+console.log('=======', github)
 
 async function chat(prompt = []) {
   const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, model });  
