b9bcfb3207b786540e5e39651d5b219c6f4a79bb
Shampongzhu <xp_zhu@qq.com>
2024-07-16 11:33:46 +0800

    Update pr-review-script.js

--- pr-review-script.js
+++ pr-review-script.js
@@ -1,15 +1,12 @@
 const { AzureOpenAI } = require("openai");
 
-// Load the .env file if it exists
 const dotenv = require("dotenv");
 dotenv.config();
 
-// You will need to set these environment variables or edit the following values
 const endpoint = 'https://chataihub3097202828.openai.azure.com/';
 const apiKey = process.env.OPENAI_APIKEY;
 const apiVersion = "2024-02-15-preview";
-const deployment = "gpt-4o"; //The deployment name for your completions API model. The instruct model is the only new model that supports the legacy API.
-
+const deployment = "gpt-4o";
 const prompt = ["When was Microsoft founded?"];
 
 async function main() {
@@ -17,7 +14,7 @@ async function main() {
 
   const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });  
 
-  const result = await client.completions.create({ prompt, model: deployment, max_tokens: 128 });
+  const result = await client.chat.completions.create({ prompt, model: deployment, max_tokens: 128 });
 
   for (const choice of result.choices) {
     console.log(choice.text);
